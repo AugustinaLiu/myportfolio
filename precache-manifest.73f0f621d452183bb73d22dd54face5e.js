712be1a71ea3c80d18459ecb8515f1d84735e47d
System Administrator <root@Aos-MacBook-Pro.local>
2020-03-10 23:14:43 -0700

    Updates

---
+++ precache-manifest.73f0f621d452183bb73d22dd54face5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14f341d0e5e022851cc88a518c639c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "411bed7d0bd4eac15077",
+    "url": "/static/css/main.c1be2f0b.chunk.css"
+  },
+  {
+    "revision": "9d3c01266ea5eea5d75d",
+    "url": "/static/js/2.87412e84.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.87412e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411bed7d0bd4eac15077",
+    "url": "/static/js/main.6cfb1442.chunk.js"
+  },
+  {
+    "revision": "76ba2de154fd6435eca6",
+    "url": "/static/js/runtime-main.a20641ec.js"
+  }
+]);
